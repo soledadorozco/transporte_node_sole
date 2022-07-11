ddfe7486030cf719734ae69b8297b8b88f5fd226
Soledad Orozco <soledadorozco8@gmail.com>
2022-07-11 11:09:53 -0300

    tabla novedades - listar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades()
     res.render('admin/novedades' ,{
         layout:'admin/layout', // admin/layout.hbs
-        persona:req.session.nombre //soledad
+        persona:req.session.nombre, //soledad
+        novedades
     });  // view/admin/login.hbs)
 });  //cierro get
 
