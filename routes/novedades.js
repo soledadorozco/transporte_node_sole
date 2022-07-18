e08919a086fdfeb662bc0433baf13dff8ebb5e6c
Soledad Orozco <soledadorozco8@gmail.com>
2022-07-18 11:50:19 -0300

    buscadord

--- routes/novedades.js
+++ routes/novedades.js
@@ -4,11 +4,20 @@ var novedadesModel = require('../models/novedadesModel');
 
 router.get('/', async function(req, res, next) {
 
-    var novedades = await novedadesModel.getNovedades();
+    // var novedades = await novedadesModel.getNovedades();
+    var novedades;
+    if(req.query.q === undefined){
+      novedades = await novedadesModel.getNovedades();
+
+    }else{
+      novedades = await novedadesModel.buscarNovedades(req.query.q);
+    }
 
     res.render('novedades',{
       isNovedades:true,
-      novedades //tengo los registros para poder imprimirlos en el hbs
+      novedades, //tengo los registros para poder imprimirlos en el hbs
+      is_search: req.query.q !== undefined,
+      q:req.query.q
     });
   })
 
